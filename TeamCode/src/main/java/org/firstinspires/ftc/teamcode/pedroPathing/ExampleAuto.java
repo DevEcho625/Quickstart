3257bc4e67a033d62e851b3d05b6abcc1d95ae1f
DevEcho625 <anuj.m.bhatia@gmail.com>
2026-06-30 01:34:32 +0530

    update constant and tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ExampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ExampleAuto.java
@@ -16,17 +16,73 @@ import static com.pedropathing.ivy.pedro.PedroCommands.*;
 import static com.pedropathing.ivy.groups.Groups.*;
 
 @Autonomous(name = "Example Auto", group = "Examples")
-public class ExampleAuto extends OpMode {
+public class ExampleAuto extends LinearOpMode {
 
     private Follower follower;
 
+    private final Pose startPose = new Pose(21, 123.5, 2.37); // Start Pose of our robot. This is against the goal facing AWAY
+    private final Pose scorePose = new Pose(50.5, 105, 2.44); // Scoring Pose of our robot.
+    private final Pose pickup1Pose = new Pose(17, 85, 3.12); // Highest (First Set) of Artifacts from the Spike Mark.
+    private final Pose pickup2Pose = new Pose(13.1, 59.5, 3.12); // Middle (Second Set) of Artifacts from the Spike Mark.
+    private final Pose pickup3Pose = new Pose(13.7, 36.7, 3.12); // Lowest (Third Set) of Artifacts from the Spike Mark.
+    private final Pose endPose = new Pose (20.6, 67.7, -1.55); // Final Pose of our robot, off the starting line
+
+    //defining our PathChains
+    private PathChain scorePreload, grabPickup1, scorePickup1, grabPickup2, scorePickup2, grabPickup3, scorePickup3, leave;
+
+    public void buildPaths() {
+        scorePreload = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, scorePose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading())
+                .build();
+
+
+        /* This is our grabPickup1 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+        grabPickup1 = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, pickup1Pose))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading())
+                .build();
+
+        /* This is our scorePickup1 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+        scorePickup1 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup1Pose, scorePose))
+                .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose.getHeading())
+                .build();
+
+        /* This is our grabPickup2 PathChain. We are using a single path with a BezierCurve (curved line). */
+        grabPickup2 = follower.pathBuilder()
+                .addPath(new BezierCurve(scorePose, new Pose(60, 54), pickup2Pose))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup2Pose.getHeading())
+                .build();
+
+        /* This is our scorePickup2 PathChain. We are using a single path with a BezierCurve (curved line). */
+        scorePickup2 = follower.pathBuilder()
+                .addPath(new BezierCurve(pickup2Pose, new Pose(60, 54), scorePose))
+                .setLinearHeadingInterpolation(pickup2Pose.getHeading(), scorePose.getHeading())
+                .build();
+
+        /* This is our grabPickup3 PathChain. We are using a single path with a BezierCurve (curved line). */
+        grabPickup3 = follower.pathBuilder()
+                .addPath(new BezierCurve(scorePose, new Pose(60, 30), pickup3Pose))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup3Pose.getHeading())
+                .build();
+
+        /* This is our scorePickup3 PathChain. We are using a single path with a BezierCurve (curved line). */
+        scorePickup3 = follower.pathBuilder()
+                .addPath(new BezierCurve(pickup3Pose,new Pose(60, 30), scorePose))
+                .setLinearHeadingInterpolation(pickup3Pose.getHeading(), scorePose.getHeading())
+                .build();
+
+        /* This is our leave PathChain. We are using a single path using a BezierLine (straight line).
+         * We use Constant Interpolation here instead of Linear*/
+        leave = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, endPose))
+                .setConstantHeadingInterpolation(scorePose.getHeading())
+                .build();
+    }
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
 
     }
-
-    @Override
-    public void loop() {
-
-    }
+    
 }
